8e956facbf60c5ab4b7015dfd0babd5fde7b46d2
Gaurav Singh <kanwasigaurav96@gmail.com>
2025-03-14 21:57:00 +0530

    Update Fibonacci Sequence Generator.py

--- Fibonacci Sequence Generator.py
+++ Fibonacci Sequence Generator.py
@@ -1,15 +1,21 @@
 def fibonacci(n):
-    sequence = []
+    """Generate the first n Fibonacci numbers starting from F(0) = 0."""
+    if n < 0:
+        raise ValueError("n must be a non-negative integer")
+    sequence = [0] * n  
     a, b = 0, 1
-    for _ in range(n):
-        sequence.append(a)
-        a, b = b, a + b
+    for i in range(n):
+        sequence[i] = a
+        a, b = b, a + b 
     return sequence
 
 if __name__ == "__main__":
     try:
         num = int(input("Enter the number of Fibonacci numbers to generate: "))
-        print("Fibonacci sequence:")
-        print(fibonacci(num))
+        if num < 0:
+            print("Please enter a non-negative integer.")
+        else:
+            print("Fibonacci sequence:")
+            print(fibonacci(num))
     except ValueError:
         print("Please enter a valid integer.")
